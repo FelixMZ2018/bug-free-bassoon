2c19e96f664419c3e961e6237564dea207398050
Felix <FelixinLondon@googlemail.com>
2021-03-11 21:04:21 +0100

    Testing

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var temp_sensor = require("node-dht-sensor");
 
 console.log("hello")
 
-sensor.read(11, 4, function(err, temperature, humidity) {
+temp_sensor.read(11, 4, function(err, temperature, humidity) {
+  console.log(err)
     if (!err) {
       console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
     }
